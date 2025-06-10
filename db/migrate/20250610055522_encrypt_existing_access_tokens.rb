664da68e71f1bb3cc58af5381433cf63343b8dd1
keita1899 <saboten18k@gmail.com>
2025-06-10 15:52:33 +0900

    refactor: 不要なコメントを削除

--- db/migrate/20250610055522_encrypt_existing_access_tokens.rb
+++ db/migrate/20250610055522_encrypt_existing_access_tokens.rb
@@ -1,22 +1,11 @@
 class EncryptExistingAccessTokens < ActiveRecord::Migration[7.1]
   def up
-    # 既存のaccess_tokenを暗号化
-    # 現在のところ、access_tokenは空の状態なので、特に処理は不要
-    # 将来的にaccess_tokenが保存されている場合は、以下のような処理が必要
-    # 
-    # Authentication.where.not(access_token: nil).find_each do |auth|
-    #   # 暗号化する前に、一時的にencryptsを無効にして読み込み
-    #   unencrypted_token = auth.read_attribute_before_type_cast(:access_token)
-    #   # encrypts有効状態で再設定（自動的に暗号化される）
-    #   auth.update_column(:access_token, unencrypted_token) if unencrypted_token.present?
-    # end
-    
-    # 現在は何もしない（既存データが存在しないため）
+    # 既存のaccess_tokenデータが存在する場合は暗号化処理が必要
+    # 現在は新規実装のため処理不要
   end
 
   def down
-    # ダウンマイグレーションは復号化のリスクがあるため、
-    # セキュリティ上の理由で実装しない
-    raise ActiveRecord::IrreversibleMigration, "暗号化されたaccess_tokenは復号化できません"
+    raise ActiveRecord::IrreversibleMigration, 
+          "セキュリティ上の理由により暗号化されたaccess_tokenは復号化できません"
   end
 end
