f9cbb033700e00336731c613b2877ba904f74091
keita1899 <saboten18k@gmail.com>
2025-06-15 06:43:56 +0900

    fix: マイグレーションのインデックス追加と削除時の条件を改善

--- db/migrate/20250614122127_improve_github_columns.rb
+++ db/migrate/20250614122127_improve_github_columns.rb
@@ -1,11 +1,11 @@
 class ImproveGithubColumns < ActiveRecord::Migration[7.1]
   def up
-    add_index :users, :github_username, unique: true
+    add_index :users, :github_username, unique: true, if_not_exists: true
 
     change_column_default :reports, :github_push_status, from: nil, to: "not_pushed"
     change_column :reports, :github_commit_sha, :string, limit: 64
     change_column :reports, :github_file_url, :text
-    add_index :reports, :github_push_status
+    add_index :reports, :github_push_status, if_not_exists: true
 
     execute <<~SQL
       UPDATE reports SET github_push_status = 'not_pushed'
@@ -14,11 +14,11 @@ class ImproveGithubColumns < ActiveRecord::Migration[7.1]
   end
 
   def down
-    remove_index :users, :github_username
+    remove_index :users, :github_username, if_exists: true
 
     change_column_default :reports, :github_push_status, from: "not_pushed", to: nil
     change_column :reports, :github_commit_sha, :string
     change_column :reports, :github_file_url, :string
-    remove_index :reports, :github_push_status
+    remove_index :reports, :github_push_status, if_exists: true
   end
 end
